25cdce6e01435d35bcad2289e7f1e33537313e5d
Shubham1-5 <singh.shbhm90@gmail.com>
2021-05-16 17:31:52 +0530

    Add_Defect file Select defect and sub-defect bug resolved

--- app/src/main/java/com/example/roaddefect/userCompleteFragment/DefectAddFragment.java
+++ app/src/main/java/com/example/roaddefect/userCompleteFragment/DefectAddFragment.java
@@ -1,16 +1,14 @@
 package com.example.roaddefect.userCompleteFragment;
 
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.roaddefect.R;
 import com.google.android.material.textfield.TextInputLayout;
@@ -28,28 +26,27 @@ public class DefectAddFragment extends Fragment {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_defect_add, container, false);
         defectSelection = v.findViewById(R.id.defectSelection);
-        defectSubSelection=v.findViewById(R.id.defectSubSelection);
+        defectSubSelection = v.findViewById(R.id.defectSubSelection);
         defectAutoComp1 = v.findViewById(R.id.defectAutoComp1);
         defectAutoComp2 = v.findViewById(R.id.defectAutoComp2);
         parentDefectItem = new String[]{"Crack", "Pothole"};
-        subDefectItem1=new String[]{"Item1","Item2","Item3"};
-        subDefectItem2=new String[]{"Item4","Item5","Item6"};
+        subDefectItem1 = new String[]{"Item1", "Item2", "Item3"};
+        subDefectItem2 = new String[]{"Item4", "Item5", "Item6"};
 
         adapter1 = new ArrayAdapter<>(getActivity(), R.layout.defect_list_dropdown, parentDefectItem);
         adapter2 = new ArrayAdapter<>(getActivity(), R.layout.defect_list_dropdown, subDefectItem1);
         adapter3 = new ArrayAdapter<>(getActivity(), R.layout.defect_list_dropdown, subDefectItem2);
-        //defectAutoComp1.setThreshold(1);
+        defectAutoComp1.setThreshold(0);
         defectAutoComp1.setAdapter(adapter1);
 
         defectAutoComp1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(parentDefectItem[position].equals("Crack")){
-                    defectAutoComp2.setThreshold(1);
+                if (parentDefectItem[position].equals("Crack")) {
+                    defectAutoComp2.setThreshold(0);
                     defectAutoComp2.setAdapter(adapter2);
-                }
-                else{
-                    defectAutoComp2.setThreshold(1);
+                } else {
+                    defectAutoComp2.setThreshold(0);
                     defectAutoComp2.setAdapter(adapter3);
                 }
             }
