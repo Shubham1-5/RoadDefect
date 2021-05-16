c8205ee8f5e2abdc911ecb6f5c0d35590b0cb7b5
Shubham1-5 <singh.shbhm90@gmail.com>
2021-05-16 14:01:17 +0530

    Added simple map with marker

--- app/src/main/java/com/example/roaddefect/userCompleteFragment/UserMapFragment.java
+++ app/src/main/java/com/example/roaddefect/userCompleteFragment/UserMapFragment.java
@@ -1,21 +1,59 @@
 package com.example.roaddefect.userCompleteFragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.example.roaddefect.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class UserMapFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_map, container, false);
+        // Initialize view
+        View view = inflater.inflate(R.layout.fragment_user_map, container, false);
+
+        //Initializing map fragment
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().
+                findFragmentById(R.id.google_map);
+
+        //Async map
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                //when map is loaded
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(@NonNull LatLng latLng) {
+                        //when clicked on map, initialize marker options
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        //set position of marker
+                        markerOptions.position(latLng);
+                        //set title of marker
+                        markerOptions.title(latLng.latitude + ":" + latLng.longitude);
+                        //remove all marker
+                        googleMap.clear();
+                        //animating to zoom the marker
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                        //add marker on map
+                        googleMap.addMarker(markerOptions);
+                    }
+                });
+            }
+        });
+
+        //Return view
+        return view;
     }
 }
